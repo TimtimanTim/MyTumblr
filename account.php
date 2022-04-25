1b85e3d118f7b15b455f51f6081d931822e4ec84
Teemothy <strawhatluffy.king12@gmail.com>
2022-04-25 16:56:32 +0800

    script php login script added

--- account.php
+++ account.php
@@ -1,3 +1,24 @@
+<?php
+//start the session
+SESSION_START();
+
+//cheking if the session variables are set or it exist
+//to check if your still logged in
+if(isset($_SESSION['ses_username'])===false){
+    header("Location: index.php?logfirst");
+
+} else if (isset($_REQUEST['logout'])===true){
+    session_destroy();
+    header("Location: index.php?logout");
+}
+
+
+?>
+
+
+
+
+
 <!doctype html>
                         <html>
                             <head>
@@ -6,24 +27,55 @@
                                 <title>Portfolio Page</title>
 
                                 <link href='css/bootstrap.min.css' rel='stylesheet'>
-
+                                <link rel="preconnect" href="https://fonts.googleapis.com">
+                                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                                <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Cairo:wght@200;400&display=swap" rel="stylesheet">
                                 <script type='text/javascript' src='js/jquery.min.js'></script>
 
                                 <style>.profile-head {
     transform: translateY(5rem)
 }
 .cover {
-    background-image: url('images/photo-profile-background.avif');
+    background-image: url('images/cover-photo.gif');
     background-size: cover;
     background-repeat: no-repeat
 }
 
 body {
-    background: #654ea3;
-    background: linear-gradient(to right, #e96443, #904e95);
-    min-height: 100vh;
-    overflow-x: hidden
-}</style>
+    background-image: url('images/background1.jpg');
+    background-size: 100%;
+    background-repeat: round;
+    overflow-x: hidden;
+
+}
+
+p {
+    font-family: 'Cairo', sans-serif;
+
+}
+
+#headtext {
+    font-family: 'Bebas Neue', cursive;
+}
+
+#cp {
+    font-family: 'Bebas Neue', cursive;
+    font-size: 10px;
+}
+
+#full-name {
+    font-family: 'Bebas Neue', cursive;
+    font-size: 30px;
+}
+
+#add {
+    font-family: 'Bebas Neue', cursive;
+    font-size: 15px;
+    position: relative;
+    right: 6px;
+}
+
+</style> 
                                 </head>
                                 <body oncontextmenu='return false' class='snippet-body'>
                                 <div class="row py-5 px-4">
@@ -32,13 +84,13 @@ body {
         <div class="bg-white shadow rounded overflow-hidden">
             <div class="px-4 pt-0 pb-4 cover">
                 <div class="media align-items-end profile-head">
-                    <div class="profile mr-3"><img src="images/photo-profile.avif" alt="..." width="150" class="rounded mb-2 img-thumbnail"><a href="#" class="btn btn-outline-dark btn-sm btn-block">Edit Profile</a>
-
-
+                    <div class="profile mr-3"><img src="images/profile-pic.jpg" alt="..." width="150" class="rounded mb-2 img-thumbnail">
+                    <a href="?logout" class="btn btn-outline-dark btn-sm btn-block">Sign out</a>
                     </div>
                     <div class="media-body mb-5 text-white">
-                        <h4 class="mt-0 mb-0">Ryan Clifford L. Perez</h4>
-                        <p class="small mb-4"> <i class="fas fa-map-marker-alt mr-2"></i>Marinduque PH
+                        <h4 class="mt-0 mb-0" id="full-name"> <?php echo $_SESSION['ses_fullname']; ?></h4>
+                        <p class="small mb-4" id="add"> <i class="fas fa-map-marker-alt mr-2"></i>
+                        <?php echo $_SESSION['ses_address']; ?>
                          </p>
                     </div>
                 </div>
@@ -46,42 +98,44 @@ body {
             <div class="bg-light p-4 d-flex justify-content-end text-center">
                 <ul class="list-inline mb-0">
                     <li class="list-inline-item">
-                        <h5 class="font-weight-bold mb-0 d-block">21,150</h5><small class="text-muted"> <i class="fas fa-image mr-1"></i>Photos</small>
+                        <h5 class="font-weight-bold mb-0 d-block">1,302</h5><small class="text-muted"> <i class="fas fa-image mr-1"></i>Photos</small>
                     </li>
                     <li class="list-inline-item">
-                        <h5 class="font-weight-bold mb-0 d-block">756,450</h5><small class="text-muted"> <i class="fas fa-user mr-1"></i>Followers</small>
+                        <h5 class="font-weight-bold mb-0 d-block">569,293</h5><small class="text-muted"> <i class="fas fa-user mr-1"></i>Followers</small>
                     </li>
                     <li class="list-inline-item">
-                        <h5 class="font-weight-bold mb-0 d-block">340</h5><small class="text-muted"> <i class="fas fa-user mr-1"></i>Following</small>
+                        <h5 class="font-weight-bold mb-0 d-block">102</h5><small class="text-muted"> <i class="fas fa-user mr-1"></i>Following</small>
                     </li>
                 </ul>
             </div>
 
             <div class="px-4 py-3">
-                <h5 class="mb-0">Expertise </h5>
+                <h5 class="mb-0" id="headtext">Expertise </h5>
                 <div class="p-4 rounded shadow-sm bg-light">
-                    <p class="font-italic mb-0">Full-stack Web Developer</p>
-                    <p class="font-italic mb-0">Mobile App Developer</p>
+                    <p class="font-italic mb-0">BSIT Student </p>
+                    <p class="font-italic mb-0">Valorant Esport Player</p> 
                     <p class="font-italic mb-0">Photographer/Videographer</p>
+                    <p class="font-italic mb-0">Freelance Editor</p>
+                    <p class="font-italic mb-0">Programming amateur</p>
                 </div>
             </div>
 
             <div class="py-4 px-4">
                 <div class="d-flex align-items-center justify-content-between mb-3">
-                    <h5 class="mb-0">Recent Captures</h5><a href="#" class="btn btn-link text-muted">Show all</a>
+                    <h5 class="mb-0" id="headtext">Recent Captures</h5><a href="#" class="btn btn-link text-muted">Show all</a>
                 </div>
                 <div class="row">
                     
-                    <div class="col-lg-6 mb-2 pr-lg-1"><img src="images/photo-1.avif" alt="" class="img-fluid rounded shadow-sm"></div>              
-                    <div class="col-lg-6 mb-2 pl-lg-1"><img src="images/photo-2.avif" alt="" class="img-fluid rounded shadow-sm"></div>                
-                    <div class="col-lg-6 pr-lg-1 mb-2"><img src="images/photo-3.avif" alt="" class="img-fluid rounded shadow-sm"></div>
-                    <div class="col-lg-6 pl-lg-1"><img src="images/photo-4.avif" alt="" class="img-fluid rounded shadow-sm"></div>
+                    <div class="col-lg-6 mb-2 pr-lg-1"><img src="images/photo-1.jpg" alt="" class="img-fluid rounded shadow-sm"></div>              
+                    <div class="col-lg-6 mb-2 pl-lg-1"><img src="images/photo-2.jpg" alt="" class="img-fluid rounded shadow-sm"></div>                
+                    <div class="col-lg-6 pr-lg-1 mb-2"><img src="images/photo-3.jpg" alt="" class="img-fluid rounded shadow-sm"></div>
+                    <div class="col-lg-6 pl-lg-1"><img src="images/photo-4.jpg" alt="" class="img-fluid rounded shadow-sm"></div>
                 </div> 
             </div>
 
             <div class="px-4 py-3">
                 <div class="p-4 rounded shadow-sm bg-light">
-                    <p class="font-italic mb-0">All Rights Reserved. Copyright 2022</p>
+                    <p class="font mb-0" id="cp" >All Rights Reserved. Copyright 2022</p>
                 </div>
             </div>
 
--- index.php
+++ index.php
@@ -1,3 +1,56 @@
+<?php 
+// start the session function
+SESSION_START();
+
+
+//create a pre defined username and password since we do not have database
+$acc_username = "Teemothy";
+$acc_password = "timothy123";
+$acc_fullname = "Timothy John U. Timtiman";
+$acc_address = "Marinduque PH";
+
+//check the current url for redirections later
+$url_add = "http://" .$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+
+// condition to know if the button is clicked
+if (isset($_REQUEST['login_button']) === true) {
+	//get the username and password from the form and compare to the predefined username and password
+	
+	
+	// if username is wrong 
+	if($_REQUEST['form_username'] != $acc_username){
+		header("Location:" .$url_add. "?notexist");
+	}
+
+	// username is correct but wrong password
+	else if ($_REQUEST['form_username'] == $acc_username && $_REQUEST['form_password'] != $acc_password){
+		header("Location: ".$url_add. "?wrongpass");
+
+	}
+
+	// username and password both exist and correct
+	else if ($_REQUEST['form_username'] == $acc_username && $_REQUEST['form_password'] == $acc_password){
+		header("Location:" .$url_add. "?success");
+
+		//create a session variable
+		$_SESSION['ses_username'] = $acc_username;
+		$_SESSION['ses_password'] = $acc_password;
+		$_SESSION['ses_fullname'] = $acc_fullname;
+		$_SESSION['ses_address'] = $acc_address;
+
+	} // end of correct username and password 
+
+
+
+}
+
+
+
+?>
+
+
+
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -5,15 +58,26 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
-	
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	
 	<link rel="stylesheet" href="css/style.css">
 
 	</head>
 	<body>
+		<style>
+			body {
+				background-image: url('images/bg-photo.gif');
+				background-size: 100%;
+				background-repeat: round;
+				overflow-x: hidden;
+				
+			}
+
+		</style>
 	<section class="ftco-section">
 		<div class="container">
 			<div class="row justify-content-center">
@@ -24,12 +88,53 @@
 		      	</div>
 		      	<h3 class="text-center mb-4">MyTumblr Login</h3>
 						
-						<form action="#" class="login-form">
+
+
+
+
+
+				<form method="POST" class="login-form">
+
 		      		<div class="form-group">
-		      			<input type="text" class="form-control rounded-left" placeholder="Username" required>
+
+
+					  <?php
+					  // alert messages
+					  if(isset($_REQUEST['notexist'])===true){
+
+							echo "<div class ='alert alert-danger' role='alert'> Username does not exist... </div>";
+
+					  } else if (isset($_REQUEST['wrongpass']) === true){
+
+							echo "<div class ='alert alert-warning' role='alert'> Incorrect Password... </div>";
+
+					  } else if (isset($_REQUEST['success']) === true){
+						  
+							echo "<div class ='alert alert-success' role='alert'> Redirecting... </div>";
+							header("Refresh: 3; url=account.php");
+
+					  } else if (isset($_REQUEST['logout']) === true){
+						
+							echo "<div class ='alert alert-info' role='alert'> Thankyou... </div>";
+					  } else if (isset($_REQUEST['logfirst']) === true){
+
+							echo "<div class ='alert alert-info' role='alert'> Please Login First... </div>";
+
+					  } else if (isset($_SESSION['ses_username']) === true){
+						
+							echo "<div class ='alert alert-info' role='alert'> You are still logged in. Please <a href='account.php'>Click here</a> to proceed. </div>";
+
+					  }
+					  
+					  ?>
+
+
+		      			<input type="text" class="form-control rounded-left" 
+						  placeholder="Username" name="form_username" required>
 		      		</div>
 	            <div class="form-group d-flex">
-	              <input type="password" class="form-control rounded-left" placeholder="Password" required>
+	              <input type="password" class="form-control rounded-left" 
+				   placeholder="Password" name="form_password" required>
 	            </div>
 	            <div class="form-group d-md-flex">
 	            	<div class="w-50">
@@ -43,9 +148,16 @@
 								</div>
 	            </div>
 	            <div class="form-group">
-	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5">Get Started</button>
+	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5" name="login_button">Get Started</button>
 	            </div>
 	          </form>
+
+
+
+
+
+
+
 	        </div>
 				</div>
 			</div>
